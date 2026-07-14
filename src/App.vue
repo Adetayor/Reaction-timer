bb3616822962398d768657e3630a4c1a05fbe2cb
Adetayor <adetayo456@gmail.com>
2026-07-14 11:27:25 +0100

    Reaction Timer Concluded

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
     <h1>Loys Reaction Timer</h1>
     <button @click="start" :disabled="isPlaying">Play</button>
-    <Block v-if="isPlaying" :delay="delay"/>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+    <!-- <p v-if="showResults">Reaction time: {{ score }} ms</p> -->
+    <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
@@ -21,6 +26,12 @@ export default {
         this.delay = 2000 + Math.random() * 5000
         this.isPlaying = true
         // console.log(this.delay)
+        this.showResults = false
+    },
+    endGame(reactionTime) {
+        this.score = reactionTime
+        this.isPlaying = false
+        this.showResults = true
     }
   }
 }
@@ -34,5 +45,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 10px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
